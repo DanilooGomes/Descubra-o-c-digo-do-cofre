5106b0d8a054ef65d3b1a0cb97a139d93c0e6a5d
DanilooGomes <34044543+DanilooGomes@users.noreply.github.com>
2019-03-03 10:44:30 -0300

    Add files via upload

---
+++ qualEASenha2.js
@@ -0,0 +1,86 @@
+let geraSenha = Math.floor(Math.random()*123456+123456).toString();
+let contador = 0;
+let senhaInformada = prompt("Informe uma senha de 6 digitos");
+
+if(senhaInformada == geraSenha){
+	document.write("Senha correta");
+}else if(senhaInformada != geraSenha && senhaInformada.charAt(0) == geraSenha.charAt(0)){
+	document.write("Código inválido, 1 número na posição certa");
+}else if(senhaInformada != geraSenha && senhaInformada.charAt(1) == geraSenha.charAt(1)){
+	document.write("Código inválido, 1 número na posição certa");
+}else if(senhaInformada != geraSenha && senhaInformada.charAt(2) == geraSenha.charAt(2)){
+	document.write("Código inválido, 1 número na posição certa");
+}else if(senhaInformada != geraSenha && senhaInformada.charAt(3) == geraSenha.charAt(3)){
+	document.write("Código inválido, 1 número na posição certa");
+}else if(senhaInformada != geraSenha && senhaInformada.charAt(4) == geraSenha.charAt(4)){
+	document.write("Código inválido, 1 número na posição certa");
+}else if(senhaInformada != geraSenha && senhaInformada.charAt(5) == geraSenha.charAt(5)){
+	document.write("Código inválido, 1 número na posição certa");
+}else if(senhaInformada != geraSenha && senhaInformada.charAt(0) == geraSenha.charAt(1)){
+	document.write("Código inválido, 1 número certo na posição errada");
+}else if(senhaInformada != geraSenha && senhaInformada.charAt(0) == geraSenha.charAt(2)){
+	document.write("Código inválido, 1 número certo na posição errada");
+}else if(senhaInformada != geraSenha && senhaInformada.charAt(0) == geraSenha.charAt(3)){
+	document.write("Código inválido, 1 número certo na posição errada");
+}else if(senhaInformada != geraSenha && senhaInformada.charAt(0) == geraSenha.charAt(4)){
+	document.write("Código inválido, 1 número certo na posição errada");
+}else if(senhaInformada != geraSenha && senhaInformada.charAt(0) == geraSenha.charAt(5)){
+	document.write("Código inválido, 1 número certo na posição errada");
+}else if(senhaInformada != geraSenha && senhaInformada.charAt(1) == geraSenha.charAt(0)){
+	document.write("Código inválido, 1 número certo na posição errada");
+}else if(senhaInformada != geraSenha && senhaInformada.charAt(1) == geraSenha.charAt(2)){
+	document.write("Código inválido, 1 número certo na posição errada");
+}else if(senhaInformada != geraSenha && senhaInformada.charAt(1) == geraSenha.charAt(3)){
+	document.write("Código inválido, 1 número certo na posição errada");
+}else if(senhaInformada != geraSenha && senhaInformada.charAt(1) == geraSenha.charAt(4)){
+	document.write("Código inválido, 1 número certo na posição errada");
+}else if(senhaInformada != geraSenha && senhaInformada.charAt(1) == geraSenha.charAt(5)){
+	document.write("Código inválido, 1 número certo na posição errada");
+}else if(senhaInformada != geraSenha && senhaInformada.charAt(2) == geraSenha.charAt(0)){
+	document.write("Código inválido, 1 número certo na posição errada");
+}else if(senhaInformada != geraSenha && senhaInformada.charAt(2) == geraSenha.charAt(1)){
+	document.write("Código inválido, 1 número certo na posição errada");
+}else if(senhaInformada != geraSenha && senhaInformada.charAt(2) == geraSenha.charAt(3)){
+	document.write("Código inválido, 1 número certo na posição errada");
+}else if(senhaInformada != geraSenha && senhaInformada.charAt(2) == geraSenha.charAt(4)){
+	document.write("Código inválido, 1 número certo na posição errada");
+}else if(senhaInformada != geraSenha && senhaInformada.charAt(2) == geraSenha.charAt(5)){
+	document.write("Código inválido, 1 número certo na posição errada");
+}else if(senhaInformada != geraSenha && senhaInformada.charAt(3) == geraSenha.charAt(0)){
+	document.write("Código inválido, 1 número certo na posição errada");
+}else if(senhaInformada != geraSenha && senhaInformada.charAt(3) == geraSenha.charAt(1)){
+	document.write("Código inválido, 1 número certo na posição errada");
+}else if(senhaInformada != geraSenha && senhaInformada.charAt(3) == geraSenha.charAt(2)){
+	document.write("Código inválido, 1 número certo na posição errada");
+}else if(senhaInformada != geraSenha && senhaInformada.charAt(3) == geraSenha.charAt(4)){
+	document.write("Código inválido, 1 número certo na posição errada");
+}else if(senhaInformada != geraSenha && senhaInformada.charAt(3) == geraSenha.charAt(5)){
+	document.write("Código inválido, 1 número certo na posição errada");
+}else if(senhaInformada != geraSenha && senhaInformada.charAt(4) == geraSenha.charAt(0)){
+	document.write("Código inválido, 1 número certo na posição errada");
+}else if(senhaInformada != geraSenha && senhaInformada.charAt(4) == geraSenha.charAt(1)){
+	document.write("Código inválido, 1 número certo na posição errada");
+}else if(senhaInformada != geraSenha && senhaInformada.charAt(4) == geraSenha.charAt(2)){
+	document.write("Código inválido, 1 número certo na posição errada");
+}else if(senhaInformada != geraSenha && senhaInformada.charAt(4) == geraSenha.charAt(3)){
+	document.write("Código inválido, 1 número certo na posição errada");
+}else if(senhaInformada != geraSenha && senhaInformada.charAt(4) == geraSenha.charAt(5)){
+	document.write("Código inválido, 1 número certo na posição errada");
+}else if(senhaInformada != geraSenha && senhaInformada.charAt(5) == geraSenha.charAt(0)){
+	document.write("Código inválido, 1 número certo na posição errada");
+}else if(senhaInformada != geraSenha && senhaInformada.charAt(5) == geraSenha.charAt(1)){
+	document.write("Código inválido, 1 número certo na posição errada");
+}else if(senhaInformada != geraSenha && senhaInformada.charAt(5) == geraSenha.charAt(2)){
+	document.write("Código inválido, 1 número certo na posição errada");
+}else if(senhaInformada != geraSenha && senhaInformada.charAt(5) == geraSenha.charAt(3)){
+	document.write("Código inválido, 1 número certo na posição errada");
+}else if(senhaInformada != geraSenha && senhaInformada.charAt(5) == geraSenha.charAt(4)){
+	document.write("Código inválido, 1 número certo na posição errada");
+}else{
+	document.write("Código inválido");
+}
+
+
+
+
+
